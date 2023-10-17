40eba6bc1a5fd2a5b1590808683830c357051dc8
Brahmadutta02 <brahmaduttadalai012@gmail.com>
2023-10-17 15:47:37 +0530

    Adding my portfolio

---
+++ src/Data/data.js
@@ -0,0 +1,38 @@
+export const data = {
+  personal_info: {
+    first_name: "Brahmadutta",
+    last_name: "Dalai",
+    email: "dalaibrahmadutta@gmail.com",
+    mobile: "+918637252035",
+    address: "Bhubaneswar, Odisha ",
+    city: " Bhubaneswar",
+    state: "Odisha",
+    postal_code: "756006",
+    objective:
+      "Front-End Developer with experience in React.js, Redux, and REST APIs, as well as a working knowledge of Node.js, a popular back-end language. I am passionate about creating dynamic, interactive web applications that provide an engaging user experience.",
+  },
+  work_experience: [
+    {
+      id: 1,
+      job_title: "Web 2 & Web 3 Developer",
+      organization_name: "AlmaBetter",
+      start_year: "2022",
+      end_year: "present",
+    },
+    {
+      id: 2,
+      job_title: "Web 2 & Web 3 Developer",
+      organization_name: "AlmaBetter",
+      start_year: "2022",
+      end_year: "present",
+    },
+  ],
+  education_details: {
+    type: "Graduation",
+    university: "Odisha",
+    degree: "BBA",
+    start_year: "2017",
+    end_year: "2020",
+  },
+  key_skills: ["ReactJs", "Javascript", "Node Js", "Redux"],
+};
